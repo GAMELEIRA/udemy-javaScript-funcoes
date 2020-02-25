7771ab898ac704e7da4e42f5246ac3780aa28dea
GAMELEIRA <gameleira270499@gmail.com>
2020-02-25 17:56:57 -0300

    Criado o arquivo thisBind.js que têm como objetivo realizar o trabalho com funções e bind.

---
+++ thisBind.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+const pessoa = {
+
+    saudacao: `Bom dia!`,
+
+    falar(){
+
+        console.log(this.saudacao);
+
+    }
+
+}
+
+pessoa.falar();
+
+const falar = pessoa.falar.bind(pessoa);
+
+falar();
