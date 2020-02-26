09dffe892bb9deb038a079f4603463e84b8c3a41
GAMELEIRA <gameleira270499@gmail.com>
2020-02-26 09:32:22 -0300

    Criado o arquivo callback3.js que têm como objetivo realizaro trabalho com funções callback

---
+++ callback3.js
@@ -0,0 +1,8 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:26/02/2020 
+ * HORA:09:03
+ * FINALIDADE DO PROGRAMA: TRABALHAR COM CALLBACK EM CORPOS DE PAGINAS 
+ */
+
+document.getElementsByTagName('body')[0].onclick = (e) => console.log(`O evento ocorreu!`);
