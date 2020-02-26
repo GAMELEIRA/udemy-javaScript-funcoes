df3feeba8e1460ae9dbdb9d5bfb385b160a6d99b
GAMELEIRA <gameleira270499@gmail.com>
2020-02-26 16:44:09 -0300

    Criado o arquivo iife.js que têm como objetivo realizar o entendimento da função auto invocada

---
+++ iife.js
@@ -0,0 +1,13 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:26/02/2020 
+ * HORA:16:40
+ * FINALIDADE DO PROGRAMA: ENTENDENDO IIFE
+ */
+
+ (function(){
+
+    console.log(`Será executado na hora!`);
+    console.log(`Gaby volta pra mim!`);
+
+ })()
