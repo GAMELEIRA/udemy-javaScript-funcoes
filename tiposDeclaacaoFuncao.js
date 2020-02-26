d978af26b2db10290e08b260185029143bb933d7
GAMELEIRA <gameleira270499@gmail.com>
2020-02-26 11:00:38 -0300

    Criado o arquivo tiposDeclaracaoFuncao.js que têm como objetivo trabalhar com declaração de funções.

---
+++ tiposDeclaacaoFuncao.js
@@ -0,0 +1,26 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:26/02/2020 
+ * HORA:09:03
+ * FINALIDADE DO PROGRAMA: TRABALHAR COM FUNCOES
+ */
+
+//POSSO CHAMAR ESSA FUNCAO EM QUALQUER LUGAR DESTE ARQUIVO
+function soma(x, y) {
+
+    return x + y;
+
+}
+
+//SO PODE SER EXECUTADA DEPOIS DE DECLARADA
+const sub = function (x, y) {
+
+    return x - y;
+
+}
+
+const mult = function mult(x,y){
+
+    return x + y;
+
+}
